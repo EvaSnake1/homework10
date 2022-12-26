e17b5bd96f29f978096bb8489c1c0eb0241904c7
«Eva» <ewa.eyrian-wolegowa@yandex.ru>
2022-12-26 20:11:48 +0300

    second commit

---
+++ data.js
@@ -0,0 +1,31 @@
+const character = `
+[
+    {"url": "https://anapioficeandfire.com/api/characters/583",
+	"name": "Jon Snow",
+	"gender": "Male",
+	"culture": "Northmen",
+	"born": "In 283 AC",
+	"died": "",
+	"titles": 
+		"Lord Commander of the Night's Watch"
+    },
+ {
+	"url": "https://anapioficeandfire.com/api/characters/12",
+	"name": "Balon Greyjoy",
+	"gender": "Male",
+	"culture": "",
+	"born": "230 AC, at Pyke",
+	"died": "",
+	"titles": "Iron King"
+ },
+    {
+        "url": "https://anapioficeandfire.com/api/characters/127",
+        "name": "Ardrian Celtigar",
+        "gender": "Male",
+        "culture": "",
+        "born": "In 229 AC or later",
+        "died": "",
+        "titles": "Lord of Claw Isle"
+    }
+]
+`
